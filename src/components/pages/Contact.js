d5714b5a025f1708ce425c62bb19c77e110e3b1b
percivalho <35653644+percivalho@users.noreply.github.com>
2023-08-20 00:05:06 +0100

    contact form, no format yet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from '../../utils/helpers';
 
-export default function Contact() {
+
+
+function Contact() {
+
+  const formData = { fullname: '', email: '', message: "" };
+  // Here we set two state variables for firstName and lastName using `useState`
+  const [data, setData] = useState(formData);
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+    // Alert the user their first and last name, clear the inputs
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(data.email) || !data.message || !data.fullname) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    //alert(`Hello ${data.firstName} ${data.lastName}`);
+    setData(formData);
+  };
+
+
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Contact Me</h1>
+      <form className="form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            value={data.fullname}
+            name="fullname"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="fullname"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            value={data.email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            value={data.message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="message"
+          />
+        </div>
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
+
+export default Contact;
