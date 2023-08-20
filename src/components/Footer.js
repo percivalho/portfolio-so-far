c7150a3af7fb5a1ecf2f7cf3ce235f84c601f605
percivalho <35653644+percivalho@users.noreply.github.com>
2023-08-20 12:59:22 +0100

    nav bar style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,55 @@
 import React from 'react';
 //import '../styles/Header.css';
+import github from '../images/github.png';
+import linkedin from '../images/linkedin.png';
+import meta from '../images/meta.png';
 
 const styles = {
     display: 'flex',
+    flexDirection: "column",
     justifyContent: 'center',
+    alignItems: 'center',
     padding: '2%',
     background: "#0096c7",
 };
 
+const socialStyle = {
+    display: 'flex',
+    flexDirection: "row",
+    justifyContent: 'center',
+    padding: "1%",
+}
+
+const imgStyle = {
+    width: 32,
+}
+
 function Footer() {
     return (
         <footer style={styles}>
+            <div style={socialStyle}>
+                <a href="https://github.com/percivalho"><img
+                    src={github}
+                    alt="github"
+                    style={imgStyle}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.6}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
+                /></a>
+                <a href="https://www.linkedin.com"><img
+                    src={linkedin}
+                    alt="linkedin"
+                    style={imgStyle}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.6}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
+                /></a>
+                <a href="https://www.facebook.com"><img
+                    src={meta}
+                    alt="meta"
+                    style={imgStyle}
+                    onMouseOver={e => e.currentTarget.style.opacity = 0.6}
+                    onMouseOut={e => e.currentTarget.style.opacity = 1}
+                /></a>
+            </div>
             <span>© 2023 Percival Ho</span>
         </footer>
     );
